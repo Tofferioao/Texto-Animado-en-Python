1a8ca67163f70ebd0cf7b9df1b9d5ea7031005af
Joao Perez <joaoespana5@gmail.com>
2025-09-09 18:57:40 -0500

    Loop animation and handle KeyboardInterrupt
    The title animation now loops indefinitely until interrupted by the user. Added a try-except block to gracefully handle KeyboardInterrupt and display a message when the animation is stopped.

--- TituloAnimado.py
+++ TituloAnimado.py
@@ -1,17 +1,20 @@
 import time
-import sys 
+import sys
 
 def animate_title(title):
-    for i in range(len(title)):
-        
-        animated_title = ''
-        for j in range(len(title)):
-            if i == j:
-                animated_title += title[j].upper()
-            else:
-                animated_title += title[j].lower()
-        print(animated_title, end='\r')
-        time.sleep(0.1)
+    try:
+        while True:
+            for i in range(len(title)):
+                animated_title = ''
+                for j in range(len(title)):
+                    if i == j:
+                        animated_title += title[j].upper()
+                    else:
+                        animated_title += title[j].lower()
+                print(animated_title, end='\r')
+                time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("\nAnimación detenida por el usuario.")
 
 title = input("Escribe algo cool: ")
 
